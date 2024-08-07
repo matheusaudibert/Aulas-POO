be5fa1a20829745d394cfe0d748f9234df75ebc8
Matheus Audibert <matheusaudibert2019@outlook.com>
2024-08-07 19:32:52 -0300

    commit

---
+++ programas/programa02.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// Exercício 1
+
+int main(void)
+{
+  int numero;
+
+  cout << "Entre uma valor:";
+  cin >> numero;
+  cout << "O nmero inserido foi" << numero << " !";
+
+  return 0;
+}
